cba9a34f489ee6fd1d8a9857ae641a10c5fb2263
Morteza3123 <mkhedry@gmail.com>
2021-11-16 00:56:05 +0330

    set uniswapsdk

---
+++ index.js
@@ -0,0 +1,15 @@
+const { ChainId, Fetcher, WETH, Route } = require('@uniswap/sdk')
+
+const chainId = ChainId.MAINNET;
+const tokenAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
+
+const init = async () => {
+    const dai = await Fetcher.fetchTokenData(chainId, tokenAddress);
+    const weth = WETH[chainId]
+    const pair = await Fetcher.fetchPairData(dai, weth);
+    const route = new Route([pair], weth);
+    console.log(route.midPrice.toSignificant(6))
+    console.log(route.midPrice.invert().toSignificant(6))
+}
+init()
+
